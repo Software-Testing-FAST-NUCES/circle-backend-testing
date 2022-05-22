436830536cd137af595cd1dfcfd64dcfee446b74
haseebshams-pixel <hadishams38.hs@gmail.com>
2022-05-23 00:21:23 +0500

    cluster added

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ var cors = require("cors");
 
 app.use(cors({ exposedHeaders: "Authorization" }));
 
-require("./startup/config")();
 require("./startup/publicFolders")(app);
+require("./startup/dotenv")();
 require("./startup/routes")(app);
 require("./startup/db")();
-require("./startup/validation")();
 
 //listener
 const port = process.env.PORT || 8000;
